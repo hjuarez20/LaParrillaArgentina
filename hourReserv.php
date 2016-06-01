062067ba709c2a54ef6320b785eef203773ae1b8
Victor Carranza Rodriguez <viccra3094@gmail.com>
2016-05-31 23:52:32 -0600

    Victor Carranza R. B31457
    Eliminacion de comentarios sin uso, correcion del formato de fecha en
    php para la base de datos asi como la correcion de la hora guardada

--- hourReserv.php
+++ hourReserv.php
@@ -1,28 +1,16 @@
 <?php 
 require 'db/db.php';
 
-
- 
-
-$countries = ["08:00am", "09:00am","10:00am","11:00am","12:00md","01:00pm"];
-
      if($_POST){
          
-       //  $fecha=date("Y-m-d",strtotime())
          $fecha=date("Y-m-d",strtotime($_POST["param"] ));
-         
-      /*   $db=$database->query("select time from tbtime where 
-         tbtime.time not in(select tbreservations.reservationHour from tbreservations where tbreservations.date='".$_POST["param"]."');")->fetchAll(PDO::FETCH_ASSOC); */
-         
+     
           $db=$database->query("select time from tbtime where 
          tbtime.time not in(select tbreservations.reservationHour from tbreservations where tbreservations.date='".$fecha."');")->fetchAll(PDO::FETCH_ASSOC);
-         
          
             createJSON($db);
       
      }
-
-
 /*------------this is a method that make the list of the hours to reserv-------------------*/
 function createJSON($data){
         $len = count($data);
