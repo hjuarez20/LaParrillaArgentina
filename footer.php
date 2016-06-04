0eefc03ce2b605c9b5ca3128a0d5df947e05699b
OscarUCR920 <oscargzsb@gmail.com>
2016-06-04 07:02:04 -0600

    Oscar 6/4/2016
    Modificaciones al Footer con el permiso de PRI PRI

--- footer.php
+++ footer.php
@@ -1,19 +1,66 @@
-<html>
+    <html>
 
-    <head>    </head>
+    <head>    
+    
+        </head>
 
     <body>
 
-        <div class="containerCenter tableSize ">
+        <div class="containerCenter tableSize">
             <section>
+                
                 <div class="row">
+                    <div class=" col-xs-12 col-sm-12 col-md-4">
+                          <h6>Información</h6>
+                          <p>Podras ponerte en contacto con nosotros por medio de cualquiera de las Redes Sociales en las que nos encontramos o si prefieres puedes dejarnos un mensaje en la seccion de Contactenos y te responderemos lo antes posible. Te esperamos.</p>
+                    </div>
+                    <div class=" col-xs-12 col-sm-12 col-md-4">
+                          <h6>Eventos</h6>
+                          <p>Servicio de Catherin's<br>
+                          Fiestas<br>
+                          Matrimonios<br>
+                          Galas<br>
+                          Conferencias<br>
+                          Y mucho mas</p>
+                    </div>
+                    <div class=" col-xs-12 col-sm-12 col-md-4">
+                         <h6>Desarrolladores</h6>
+                         <p><img src="img/developers/priscila.png"> (CEO) Priscilla Jimenez<br>
+                         <img src="img/developers/harold.png">
+                         (Php Senior) Harold Juarez<br>
+                         <img src="img/developers/andrea.png">
+                         (Web/Designer) Andrea Palma<br>
+                         <img src="img/developers/andrey.png">
+                         (Web/Designer) Andrey Aguirre<br>
+                         <img src="img/developers/oscar.png">
+                         (Web/QA) Oscar Noguera<br>
+                         <img src="img/developers/victor.png">
+                         (Web/QA) Victor Carranza<br>
+                         <img src="img/developers/nelson.png">
+                         (Web Dev) Nelson Guadamuz<br>
+                         <img src="img/developers/ricardo.png">
+                         (Web Dev) Ricardo The Cat<br>
+                         
+                         
+                         </p>
+                         
+                    </div>
+                </div>
+                
+                    <div class="row">
                     <div class="col-xs-12">
                         <div class="table-responsive">
-                            <a class="icons" href="https://www.facebook.com/La-Parrilla-Argentina-1354162611267584/?skip_nax_wizard=true" target="_blank"><img alt="siguenos en facebook" height="30" src="img/facebook.png" title="siguenos en facebook" width="30" /></a>
-                            <a href="https://twitter.com/?request_context=signup" target="_blank"><img alt="siguenos en Twitter" height="30" src="img/twitter.png" title="siguenos en Twitter" width="30" /></a>
-                            <p>©Todos los derechos reservados-Proyecto Multimedios 2016</p>
+                            <a class="icons" href="https://www.facebook.com/La-Parrilla-Argentina-1354162611267584/?skip_nax_wizard=true" target="_blank"><img alt="siguenos en facebook" height="30" src="img/social/facebook.png" title="siguenos en facebook" width="30" /></a>
+                            <a class="icons" href="https://twitter.com/?request_context=signup" target="_blank"><img alt="siguenos en Twitter" height="30" src="img/social/twitter.png" title="siguenos en Twitter" width="30" /></a>
+                             <a class="icons" href="https://youtube.com" target="_blank"><img alt="siguenos en Twitter" height="30" src="img/social/youtube.png" title="siguenos en Youtube" width="30" /></a>
+                              <a class="icons" href="https://pinterest.com" target="_blank"><img alt="siguenos en Pinterest" height="30" src="img/social/pinterest.png" title="siguenos en Twitter" width="30" /></a>
+                               <a class="icons" href="https://linkedin.com" target="_blank"><img alt="siguenos en LinkedIn" height="30" src="img/social/linkedin.png" title="siguenos en Twitter" width="30" /></a>
                         </div>
                     </div>
+                </div>
+                
+                <div class="row">
+                    <p>®Todos los derechos reservados - Proyecto Multimedios 2016</p>
                 </div>
             </section>
         </div>
