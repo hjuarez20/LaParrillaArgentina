6f5b24f827e7c6a9ee833f8298b3174ac1d97c67
Victor Carranza Rodriguez <viccra3094@gmail.com>
2016-06-06 15:30:38 -0600

    Victor Carranza B31457
    Cambios con la modificacion del ingreso de platillos

---
+++ foodMenuSettings.php
@@ -0,0 +1,41 @@
+<?php 
+require 'db/db.php';
+
+if($_GET){
+    $id=$_GET["id"];
+    
+}
+
+if(isset($_POST['saveChanges'])){
+     /*--------------------section just for saving the image in the system----------*/
+        $name= $_FILES['txtImage']['name'];
+        $tmp= $_FILES['txtImage']['tmp_name'];
+        $folder='dbImgs';
+        //the files are saved in a temp folder
+        move_uploaded_file($tmp,$folder.'/'.$name);
+        $imagePath=($folder.'/'.$name);
+        /*******************************************************************************/
+
+}
+    
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Calendario</title>
+</head>
+<body>
+     
+       
+        <form method="post" action="foodMenuSettings.php" enctype="multipart/form-data">
+           </br>
+           <label>Seleccione una imagen</label>
+    <input type="file" name="txtImage"/><br/><br/>
+            <button type="submit" name="saveChanges">Guardar Cambios</button>
+        </form>
+</body>
--- principal.php
+++ principal.php
@@ -23,17 +23,6 @@ if($_POST){
          isset($_POST['image']) && ($_POST['image'])!=null
         ){/*TODO ESTE IF ANTERIOR ES POR SEGURIDAD*/
      
-          /*--------------------section just for saving the image in the system----------
-        $name= $_FILES['image']['name'];
-          echo($name);
-        $tmp= $_FILES['image']['tmp_name'];
-        $folder='dbImgs';
-        //the files are saved in a temp folder
-        move_uploaded_file($tmp,$folder.'/'.$name);
-        $imagePath=($folder.'/'.$name);
-       // echo $imagePath;
-        /*******************************************************************************/
-    
     $database->insert("tbmenu", [
         "idUserMod" => $_SESSION['idUser'],
         "nameDish" => $_POST["nameDish"],
