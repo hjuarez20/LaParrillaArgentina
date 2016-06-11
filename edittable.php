20e8af1a1425d12d6c17734e172c47a34a99ebea
Harold <hjuarez20@gmail.com>
2016-06-11 00:32:51 -0600

    Harold 11/06/2016
    ya edita reservaciones, falta usuarios, lo hace desde un mismo archivo

--- edittable.php
+++ edittable.php
@@ -1,5 +1,25 @@
 <?php
 require "db/db.php";
+
+if($_POST){
+    
+    if($_POST["value"] == 1){
+        $database->update("tbreservations",
+                          ["date" => $_POST["date"],
+                           "peopleAmount" => $_POST["peopleAmount"]],
+                         ["idReservation" => $_POST["id"]]);
+        header ("Location: principal.php#contReservation");
+   }
+
+   if($_POST["value"] == 3){
+        $database->update("tbusers",
+                          ["login" => $_POST["login"],
+                           "peopleAmount" => $_POST["password"]],
+                         ["idUser" => $_POST["id"]]);
+        header ("Location: principal.php#contClient");
+   }
+    
+}
 if($_GET){
     $type = $_GET["type"]; 
     $id = $_GET["id"];
@@ -13,26 +33,14 @@ if($_GET){
 
      $value=1;
     }
-    if($type == 2){
-        echo "hola menu";
-        $value=2;
-    }
+
     if($type == 3){
         $data = $database->select("tbusers", "*", ["idUser" => $id]);
         $value=3;
     }
 }
 
-if($_POST){
-   /* $id= $_GET['idedit'];
-    if($id == 1){*/
-        $dataBase->update("tbreservations",
-                          ["date" => $_POST["date"],
-                           "peopleAmount" => $_POST["peopleAmount"]],
-                         ["idReservation" => $_POST["id"]]);
-       // header ("Location: principal.php");
-   // }
-}
+
 ?>
 <html>
 <body>
@@ -45,37 +53,30 @@ if($_POST){
         <input name='peopleAmount' value=".$data[0]["peopleAmount"]."><br>
         <label>Hora de reservation</label>
         <input name='' value=".$data[0]["reservationHour"]."><br>
-        <label>Estado</label>
+        <label>Fecha</label>
         <input name='date' value=".$data[0]["date"].">
         <input type='hidden' name='id' value=".$data[0]["idReservation"].">
+        <input type='hidden' name='value' value=1>
         <input type='submit' value='EDITAR'>
         <input type='button' value='CANCELAR' onclick='history.back();'>
     </form>";
    }
-    
-   if($value == 2){
-   echo "<form action='edittable.php?id=1'>
-        <label>Nombre</label>
-        <input name='' value=".$data[0]["clientName"]."><br>
-        <label>Cantidad mesas</label>
-        <input name='' value=".$data[0]["peopleAmount"]."><br>
-        <label>Hora de reservation</label>
-        <input name='' value=".$data[0]["reservationHour"]."><br>
-        <label>Estado</label>
-        <input name='' value=".$data[0]["date"].">
-    </form>";
-   }
  
    if($value == 3){
-   echo "<formaction='edittable.php?id=3'>
+    $password= md5($data[0]["password"]);
+   echo "<form  action='edittable.php' method='post'>
         <label>Nombre</label>
-        <input name='' value=".$data[0]["login"]."><br>
+        <input name='' value=".$data[0]["name"]."><br>
         <label>Cantidad mesas</label>
-        <input name='' value=".md5($data[0]["password"])."><br>
+        <input name='password' value=".$password."><br>
         <label>Hora de reservation</label>
         <input name='' value=".$data[0]["identification"]."><br>
         <label>Nombre Usuario</label>
-        <input name='' value=".$data[0]["name"].">
+        <input name='login' value=".$data[0]["login"].">
+        <input type='hidden' name='id' value=".$data[0]["idUser"].">
+        <input type='hidden' name='value' value=3>
+        <input type='submit' value='EDITAR'>
+        <input type='button' value='CANCELAR' onclick='history.back();'>
     </form>";
    }
  ?>
