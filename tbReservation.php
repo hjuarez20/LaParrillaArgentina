94cda70599ea2e51eb0106d1ecc2af90915cfdb6
NELGUARO <nelguaro@gmail.com>
2016-06-22 02:06:39 -0600

    eliminar tbusers tbreservations
    22/6/16

--- tbReservation.php
+++ tbReservation.php
@@ -3,13 +3,10 @@
 // require 'db/db.php';
  
 $data = $database->select("tbreservations", "*");
- 
 ?>
  <html>
 
-    <head>    
-<link href="css/principal.css" rel="stylesheet" type="text/css">
-    </head>
+    <head>    </head>
 
     <body>
 
@@ -19,14 +16,16 @@ $data = $database->select("tbreservations", "*");
                     <div class="col-xs-12">
                         <div class="table-responsive">
                             <article class="tbl-header">
-                                <table cellpadding="0" cellspacing="0" border="0" >
+                                <table cellpadding="0" cellspacing="0" border="0">
                                     <tr>
                                         <th>Nombre</th>
                                         <th>Teléfono</th>
                                         <th>Email</th>
                                         <th>Fecha</th>
                                         <th>Cantidad de Personas</th>
+                                        <th>Estado</th>
                                         <th>Opciones</th>
+                                        
                                     </tr>
                                 </table>
                             </article>
@@ -35,7 +34,15 @@ $data = $database->select("tbreservations", "*");
                                     <?php
                                     $len = count($data);
                                     for($i=0; $i<$len; $i++){
-                                    echo  "<tr><td>".$data[$i]["clientName"]."</td><td>".$data[$i]["clientPhone"]."</td><td>".$data[$i]["clientEmail"]."</td><td>".$data[$i]["date"]."</td><td>".$data[$i]["peopleAmount"]."</td><td><a href='edittable.php?type=1&id=".$data[$i]["idReservation"]."'>Editar</a> <a href='delete.php?id=".$data[$i]["idReservation"]."'>Eliminar</a></td></tr>";
+                                        if($data[$i]["state"]==1){
+                                            $state="Activo";
+                                        }else{
+                                            $state="Terminado";
+                                        }
+                                        
+                                    echo  "<tr><td>".$data[$i]["clientName"]."</td><td>".$data[$i]["clientPhone"]."</td><td>".$data[$i]["clientEmail"]."</td><td>".$data[$i]["date"]."</td><td>".$data[$i]["peopleAmount"]."
+                                    <td>".$state    ."</td>
+                                    </td><td><a href='edittable.php?type=1&id=".$data[$i]["idReservation"]."'>Editar</a> <a href='eliminator.php?type=1&id=".$data[$i]["idReservation"]."'>Eliminar</a></td></tr>";
                 }
             ?>
                                 </table>
